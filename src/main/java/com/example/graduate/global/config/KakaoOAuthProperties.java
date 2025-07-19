01d27e7b30e5d483d022dfaf4b7fde5233e433fe
김나경 <1030n@naver.com>
2025-07-20 04:01:51 +0900

    :sparkles: 카카오 OAuth 설정 및 RestTemplate 빈 등록

---
+++ src/main/java/com/example/graduate/global/config/KakaoOAuthProperties.java
@@ -0,0 +1,16 @@
+package com.example.graduate.global.config;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+@ConfigurationProperties(prefix = "oauth.kakao")
+@Getter
+@Setter
+public class KakaoOAuthProperties {
+  private String clientId;
+  private String clientSecret;
+  private String redirectUri;
+  private String tokenUri;
+  private String userInfoUri;
+}
