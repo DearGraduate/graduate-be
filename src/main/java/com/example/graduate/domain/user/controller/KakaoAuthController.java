049619f2b69ade03598eccbce210ac99b8074206
김나경 <1030n@naver.com>
2025-07-20 04:17:06 +0900

    :sparkles: API endpoint에 /api/ 추가

--- src/main/java/com/example/graduate/domain/user/controller/KakaoAuthController.java
+++ src/main/java/com/example/graduate/domain/user/controller/KakaoAuthController.java
@@ -20,7 +20,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/auth/kakao")
+@RequestMapping("/api/auth/kakao")
 @RequiredArgsConstructor
 public class KakaoAuthController {
   private final KakaoOAuthService kakaoService;
