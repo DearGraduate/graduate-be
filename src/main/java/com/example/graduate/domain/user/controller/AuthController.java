1c6f3b016f1b1b72d07447982232ea6b9e27d1e1
김나경 <1030n@naver.com>
2025-07-20 19:34:56 +0900

    :sparkles: AccessToken 재발급 Controller 추가

---
+++ src/main/java/com/example/graduate/domain/user/controller/AuthController.java
@@ -0,0 +1,32 @@
+package com.example.graduate.domain.user.controller;
+
+import com.example.graduate.domain.user.exception.UserSuccessStatus;
+import com.example.graduate.domain.user.service.UserService;
+import com.example.graduate.global.apiPayload.ApiResponse;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/auth")
+@Tag(name = "Auth API", description = "JWT 인증, 재발급, 로그아웃, 탈퇴 관련 기능을 제공합니다.")
+public class AuthController {
+
+  private final UserService userService;
+
+  @Operation(
+      summary = "Access 토큰 재발급",
+      description = "Refresh 토큰을 통해 새로운 Access 토큰을 발급합니다.")
+  @PostMapping("/refresh")
+  public ApiResponse<Void> refreshToken(HttpServletRequest request, HttpServletResponse response) {
+    userService.reissueToken(request, response);
+
+    return ApiResponse.of(UserSuccessStatus.SUCCESS_REISSUE);
+  }
+}
