75f2cae67d062024da577a5ebfe0b2655a0cdbd5
김나경 <1030n@naver.com>
2025-07-21 04:04:29 +0900

    :sparkles: SecurityContextHolder에서 현재 사용자 조회하는 SecurityUtil 추가

--- src/main/java/com/example/graduate/domain/user/exception/UserErrorStatus.java
+++ src/main/java/com/example/graduate/domain/user/exception/UserErrorStatus.java
@@ -18,7 +18,7 @@ public enum UserErrorStatus implements BaseErrorCode {
   TOKEN_INVALID("AUTH4007", "유효하지 않은 리프레시 토큰입니다.", HttpStatus.UNAUTHORIZED),
   TOKEN_EXPIRED("AUTH4008", "리프레시 토큰이 만료되었습니다.", HttpStatus.UNAUTHORIZED),
   TOKEN_FAIL("AUTH4009", "토큰 파싱 또는 추출에 실패했습니다.", HttpStatus.BAD_REQUEST),
-
+  INVALID_AUTHENTICATION("AUTH4010", "인증 정보가 유효하지 않습니다.", HttpStatus.UNAUTHORIZED),
   ;
 
   private final String code;
