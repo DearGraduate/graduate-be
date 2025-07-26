50998da2a34b46dc568f8e1aaea7618b118669d3
sinyoung6491 <sinyoung6491@gmail.com>
2025-07-26 12:26:33 +0900

    ✨ Feat: 앨범 기간 삭제 축하글 삭제 추가

--- src/main/java/com/example/graduate/domain/album/scheduler/AlbumCleanupScheduler.java
+++ src/main/java/com/example/graduate/domain/album/scheduler/AlbumCleanupScheduler.java
@@ -1,6 +1,12 @@
 package com.example.graduate.domain.album.scheduler;
 
+import com.example.graduate.domain.album.domain.Album;
 import com.example.graduate.domain.album.repository.AlbumRepository;
+import com.example.graduate.domain.album.status.AlbumErrorStatus;
+import com.example.graduate.domain.letter.repository.LetterRepository;
+import com.example.graduate.domain.user.entity.User;
+import com.example.graduate.global.SecurityUtil;
+import com.example.graduate.global.apiPayload.exception.GeneralException;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -15,14 +21,28 @@ import java.util.List;
 @Slf4j
 public class AlbumCleanupScheduler {
     private final AlbumRepository albumRepository;
+    private final LetterRepository letterRepository;
+    private final SecurityUtil securityUtil;
+
+    private Long getCurrentUserId() {
+        User user = securityUtil.getCurrentUser();
+        return user.getId();
+    }
+
 
     @Scheduled(cron = "0 0 0 * * *") // 매일 00:00:00에 실행
     @Transactional
     public void deleteExpiredAlbums() {
+        Long userId = getCurrentUserId();
+
         LocalDate oneMonthAgo = LocalDate.now().minusMonths(1);
         List<com.example.graduate.domain.album.domain.Album> expiredAlbums = albumRepository.findByGraduationDateBefore(oneMonthAgo);
 
+        Album album = albumRepository.findByUserId(userId)
+                .orElseThrow(() -> new GeneralException(AlbumErrorStatus._ALBUM_NOT_FOUND));
+
         if (!expiredAlbums.isEmpty()) {
+            letterRepository.deleteAllByAlbumId(album.getId());
             albumRepository.deleteAll(expiredAlbums);
             log.info("만료된 앨범 {}개 삭제됨", expiredAlbums.size());
         } else {
