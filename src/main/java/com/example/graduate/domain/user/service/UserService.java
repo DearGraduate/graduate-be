aa384d7f2e38b8b08372d0a75710aa51aef80a65
김나경 <1030n@naver.com>
2025-08-01 01:16:45 +0900

    :recycle: 함수에서 secureCookie 인자 제거 및 내부 설정값 사용

--- src/main/java/com/example/graduate/domain/user/service/UserService.java
+++ src/main/java/com/example/graduate/domain/user/service/UserService.java
@@ -138,18 +138,19 @@ public class UserService {
    * @param maxAge  쿠키 유효 기간 (초 단위)
    * @return 생성된 ResponseCookie
    */
-  private ResponseCookie createCookie(String key, String value, long maxAge, boolean secureCookie) {
+  private ResponseCookie createCookie(String key, String value, long maxAge) {
     ResponseCookie.ResponseCookieBuilder builder = ResponseCookie.from(key, value)
         .httpOnly(true)
         .secure(secureCookie)
         .path("/")
         .maxAge(maxAge);
 
-    // prod일 때만 크로스 도메인 문제 해결
+    // prod 환경이면 SameSite + domain 설정 추가
     if (secureCookie) {
       builder.domain("photory.site");
       builder.sameSite("None");
     }
+
     return builder.build();
   }
 
