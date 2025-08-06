58ae9f91e51fe08f5a7fb1d390fc478f6d039aba
김나경 <1030n@naver.com>
2025-08-06 14:04:05 +0900

    :bug: Cors 오류 수정

--- src/main/java/com/example/graduate/global/config/CorsConfig.java
+++ src/main/java/com/example/graduate/global/config/CorsConfig.java
@@ -15,7 +15,7 @@ public class CorsConfig {
         CorsConfiguration config = new CorsConfiguration();
         config.setAllowCredentials(true); // 쿠키, 인증정보 포함 여부
         config.setAllowedOrigins(List.of(
-                "http://localhost:5173",
+
                 "http://localhost:3000",
                 "http://photory.site",
                 "https://photory.site",
