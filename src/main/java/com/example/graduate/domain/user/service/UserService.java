9344713bcf9f69b1a75fc76f63a2b314ae47a761
Jooyoung <113746577+kimjy0117@users.noreply.github.com>
2025-08-15 15:56:34 +0900

    🔨 토큰 만료시간 수정

--- src/main/java/com/example/graduate/domain/user/service/UserService.java
+++ src/main/java/com/example/graduate/domain/user/service/UserService.java
@@ -50,10 +50,10 @@ public class UserService {
         user.getSocialId(),
         user.getName(),
         null,
-        1000 * 60 * 60L // 예시: 1시간
+        1000 * 60 * 60 * 24 * 30L // 예시: 30일
     );
 
-    long refreshExpirationMs = 1000L * 60 * 60 * 24 * 7; // 7일
+    long refreshExpirationMs = 1000L * 60 * 60 * 24 * 30; // 30일
     String refresh = jwtUtil.createJwt(
         "refresh",
         user.getSocialId(),
